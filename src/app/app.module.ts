74d13eddc8c59504e832d1089b91fa7ba8953c0a
mclovin <mathieu.ford@gmail.com>
2024-03-04 18:09:25 -0500

    Created Hero-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeroesComponent } from "./heroes/heroes.component";
 import { FormsModule } from "@angular/forms";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { CommonModule } from "@angular/common";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, CommonModule],
 })
 export class AppModule {}
