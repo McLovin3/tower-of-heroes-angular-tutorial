ebe4b76843b17f0554415ce344e16c1119ca6c17
mclovin <mathieu.ford@gmail.com>
2024-03-04 19:02:20 -0500

    Added message component & service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { FormsModule } from "@angular/forms";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { CommonModule } from "@angular/common";
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
   providers: [],
   bootstrap: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, CommonModule],
